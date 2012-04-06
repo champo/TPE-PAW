01d0794d6e54b2ab36ca9dcf71795d1dfa3c211b
acrespo <alvaro.andres.crespo@gmail.com>
2012-04-06 19:39:21 -0300

    Added constructor to PropertyServiceImpl.

--- src/main/java/ar/edu/itba/paw/grupo1/service/PropertyServiceImpl.java
+++ src/main/java/ar/edu/itba/paw/grupo1/service/PropertyServiceImpl.java
@@ -2,9 +2,16 @@ package ar.edu.itba.paw.grupo1.service;
 
 import java.util.List;
 
+import ar.edu.itba.paw.grupo1.dao.PropertyDao;
 import ar.edu.itba.paw.grupo1.model.Property;
 
-public class PropertyServiceImpl implements PropertyService{
+public class PropertyServiceImpl implements PropertyService {
+
+	private PropertyDao propertyDao;
+
+	public PropertyServiceImpl(PropertyDao propertyDao) {
+		this.propertyDao = propertyDao;
+	}
 
 	public List<Property> getAll() {
 		// TODO Auto-generated method stub
