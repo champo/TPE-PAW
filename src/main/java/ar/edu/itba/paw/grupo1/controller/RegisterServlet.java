f558065b4c2b3fb30062ac749820b3021d619c58
Juan Civile <elementohb@gmail.com>
2012-04-06 19:03:57 -0300

    Validate the fields in the register form

--- src/main/java/ar/edu/itba/paw/grupo1/controller/RegisterServlet.java
+++ src/main/java/ar/edu/itba/paw/grupo1/controller/RegisterServlet.java
@@ -19,6 +19,88 @@ public class RegisterServlet extends LayoutServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		
+		boolean error = false;
+		
+		error |= !checkParameter(req, "name", 0, 50);
+		error |= !checkParameter(req, "surname", 0, 50);
+		error |= !checkParameter(req, "username", 0, 50);
+		error |= !hasParameter(req, "password");
+		error |= !areParamsEqual(req, "password", "passwordConfirmation");
+		error |= !checkEmail(req, "email", 0, 50);
+		error |= !checkPhone(req, "phone", 0, 20);
+		
+		if (error) {
+			render(req, resp, "register.jsp", "Register");
+		} else {
+			//TODO: Register the user
+		}
+	}
+	
+	protected boolean checkPhone(HttpServletRequest req, String param, int min, int max) {
+		
+		if (!checkParameter(req, param, min, max)) {
+			return false;
+		} else {
+			try {
+				Integer.valueOf(req.getParameter(param));
+				return true;
+			} catch (NumberFormatException e) {
+				req.setAttribute(param + "InvalidFormat", true);
+				return false;
+			}
+		}
+	}
+
+	protected boolean checkEmail(HttpServletRequest req, String param, int min,	int max) {
+		
+		if (!checkParameter(req, param, min, max)) {
+			return false;
+		} else if (req.getParameter(param).matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$")) {
+			return true;
+		} else {
+			req.setAttribute(param + "InvalidFormat", true);
+			return false;
+		}
+	}
+
+	protected boolean areParamsEqual(HttpServletRequest req, String param1, String param2) {
+		
+		String value1 = req.getParameter(param1);
+		String value2 = req.getParameter(param2);
+		
+		if (value1 == null || value2 == null) {
+			return false;
+		} else if (value1.equals(value2)) {
+			return true;
+		} else {
+			req.setAttribute(param2 + "DoesntMatch", true);
+			return false;
+		}
+	}
+	
+	protected boolean hasParameter(HttpServletRequest req, String param) {
+		
+		String value = req.getParameter(param);
+		if (value == null || value.length() == 0) {
+			req.setAttribute(param + "Empty", true);
+			return false;
+		}
+		
+		return true;
+	}
+	
+	protected boolean checkParameter(HttpServletRequest req, String param, int min, int max) {
+		
+		String value = req.getParameter(param);
+		if (value == null || value.length() == 0) {
+			req.setAttribute(param + "Empty", true);
+			return false;
+		} else if (value.length() < min || value.length() > max) {
+			req.setAttribute(param + "BadLength", true);
+			return false;
+		}
+		
+		return true;
 	}
 	
 }
