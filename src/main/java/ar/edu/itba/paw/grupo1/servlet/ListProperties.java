b9e9f2b83ad75386f349a3cd0ff6ef0070fbe3e4
acrespo <alvaro.andres.crespo@gmail.com>
2012-04-06 19:46:54 -0300

    Created ListProperties servlet.

---
+++ src/main/java/ar/edu/itba/paw/grupo1/servlet/ListProperties.java
@@ -0,0 +1,33 @@
+package ar.edu.itba.paw.grupo1.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import ar.edu.itba.paw.grupo1.ApplicationContainer;
+import ar.edu.itba.paw.grupo1.service.PropertyService;
+
+@SuppressWarnings("serial")
+public class ListProperties extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+
+		PropertyService propService = ApplicationContainer.getInstance().getObject(PropertyService.class);
+			
+		req.setAttribute("properties", propService.getAll() );
+		req.getRequestDispatcher("/WEB-INF/jsp/listProperties.jsp").forward(req,
+				resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req, resp);
+	}
+	
+	
+}
