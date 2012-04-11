3b71a292578865b1dfeb6dfc8bd66af250546f03
Dario Sneidermanis <dariosn@gmail.com>
2012-04-11 14:32:01 -0300

    query view 'finished' (has to be integrated with alvaritou's dao)

--- src/main/java/ar/edu/itba/paw/grupo1/controller/QueryServlet.java
+++ src/main/java/ar/edu/itba/paw/grupo1/controller/QueryServlet.java
@@ -1,21 +1,93 @@
+package ar.edu.itba.paw.grupo1.controller;
+
 import java.io.IOException;
+import java.util.ArrayList;
 
-import ar.edu.itba.paw.grupo1.controller.BaseServlet.java;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import ar.edu.itba.paw.grupo1.controller.BaseServlet;
 
 
-public class QueryServlet extends java {
+public class QueryServlet extends BaseServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		
-		render(req, resp, "query.jsp", "Query");
-	}
-	
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
+		String operation = req.getParameter("operation");
+		String property = req.getParameter("property");
+		String rangeFrom = req.getParameter("range_from");
+		String rangeTo = req.getParameter("range_to");
+		
+		if (operation == null && property == null && rangeFrom == null && rangeTo == null ) {
+			render(req, resp, "query.jsp", "Query");
+			return;
+		}
+		
+		if (operation == null) {
+			operation = "any";
+		}
+		if (property == null) {
+			property = "any";
+		}
 
+		double parsedRangeFrom = 0;
+		double parsedRangeTo = 0;
+		boolean badParse = false;
+		
+		try {
+			if (rangeFrom != null) {
+				parsedRangeFrom = Double.parseDouble(rangeFrom);
+			}
+			if (rangeTo != null) {
+				parsedRangeTo = Double.parseDouble(rangeTo);
+			}
+		} catch(NumberFormatException e) {
+			badParse = true;
+		}
+
+		if ( badParse ||
+			 ( rangeFrom != null && parsedRangeFrom < 0 ) ||
+             ( rangeTo != null && parsedRangeTo < 0 ) ||
+             ( rangeFrom != null && rangeTo != null && parsedRangeTo < parsedRangeFrom ) ){
+			
+			req.setAttribute("invalidRange", true);
+			req.setAttribute("queryResults", new ArrayList<String>());
+			render(req, resp, "query.jsp", "Query");
+			return;
+		}
+		
+		if (rangeFrom == null) {
+			parsedRangeFrom = 0;
+		}
+		if (rangeTo == null) {
+			parsedRangeTo = Double.MAX_VALUE;
+		}
+		
+		String query = "SELECT operationType, propertyType, address, neighbourhood, price FROM properties WHERE ";
+
+		if (operation.equals("selling")) {
+			query += "operationType = 0 AND ";
+		} else if (operation.equals("leasing")) {
+			query += "operationType = 1 AND ";
+		}
+		
+		if (property.equals("house")) {
+			query += "propertyType = 0 AND ";
+		} else if (property.equals("flat")) {
+			query += "propertyType = 1 AND ";
+		}
+		
+		query += "price >= " + parsedRangeFrom + " AND price <= " + parsedRangeTo;
+		
+		// TODO: actual query
+
+		// render query result
+
+		
 		render(req, resp, "query.jsp", "Query");
 	}
+
 }
--- src/main/webapp/WEB-INF/query.jsp
+++ src/main/webapp/WEB-INF/query.jsp
@@ -2,3 +2,46 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn" %>
 
 <h1>Query</h1>
+
+<div>
+	<c:if test="${invalidRange}">
+		<p class="error">Invalid price range.</p>
+	</c:if>
+	
+	<form action="/query" method="get">
+	
+		<label for="operation">Type of operation: </label>
+		<input type="radio" name="operation" value="any" checked="checked">Any
+		<input type="radio" name="operation" value="selling" />Selling
+		<input type="radio" name="operation" value="leasing" />Leasing
+		<br />
+		
+		<label for="property">Type of property: </label>
+		<input type="radio" name="property" value="any" checked="checked">Any
+		<input type="radio" name="property" value="flat" />Flat
+		<input type="radio" name="property" value="house" />House
+		<br />
+		
+		<label for="range">Price range:
+		from <input type="text" name="rangeFrom"> to <input type="text" name="rangeTo">
+		<br />
+		
+		<input type="submit" value="Query">
+	
+	</form>
+</div>
+<br />
+
+<div>
+	<c:forEach var="result" items="queryResults">
+		<p>
+			<ul>
+				<li>${result.operationType}</li>
+				<li>${result.propertyType}</li>
+				<li>${result.address}</li>
+				<li>${result.neighbourhood}</li>
+				<li>${result.price}</li>				
+			</ul>
+		</p>
+	</c:forEach>
+</div>
