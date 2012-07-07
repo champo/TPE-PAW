a7a13610aac2842e36535bd02a09b8dd79265b3d
acrespo <alvaro.andres.crespo@gmail.com>
2012-07-07 18:57:55 -0300

    Remove every mention of the wrapper Service class which is no longer used.

--- src/main/java/ar/edu/itba/paw/grupo1/web/WicketUtils.java
+++ src/main/java/ar/edu/itba/paw/grupo1/web/WicketUtils.java
@@ -27,32 +27,5 @@ public class WicketUtils {
 		EnumChoiceRenderer<T> choiceRenderer = new EnumChoiceRenderer<T>(container);
 		List<? extends T> choices = Arrays.asList(selectOptions);
 		container.add(new DropDownChoice<T>(id, choices, choiceRenderer).setRequired(true));
-	}
-
-	public static SortedSet<Service> getServices(Set<Services> propertyServices) {
-		Comparator<Service> comparator = new Comparator<Service>() {
-			public int compare(Service a, Service b) {
-				return a.getName().compareTo(b.getName());
-			}
-		};
-		
-		SortedSet<Service> services = new TreeSet<Service>(comparator);
-		
-		for (Services service : Services.values()) {
-			services.add(new Service(service.toString(), propertyServices.contains(service)));
-		}
-	
-		return services;
-	}
-	
-	public static  Set<Services> toEnumSet(List<Service> servicesList) {
-		HashSet<Services> set = new HashSet<Services>();
-		
-		for (Service s: servicesList) {
-			set.add(s.ToEnum());
-		}
-		
-		return set;
-	}
-	
+	}	
 }
