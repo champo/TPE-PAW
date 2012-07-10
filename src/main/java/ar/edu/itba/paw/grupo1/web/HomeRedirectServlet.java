648107095947e506be35134de8c945819a0fceee
Juan Pablo Civile <elementohb@gmail.com>
2012-07-10 15:55:01 -0300

    This should, in theory, make /bin be the landing

---
+++ src/main/java/ar/edu/itba/paw/grupo1/web/HomeRedirectServlet.java
@@ -0,0 +1,26 @@
+package ar.edu.itba.paw.grupo1.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.UnavailableException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HomeRedirectServlet extends HttpServlet  {
+	
+	private static final String HOME_URL_PARAM = "homeUrl";
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		String target = getInitParameter(HOME_URL_PARAM);
+		if (target == null) {
+			throw new UnavailableException("No home has been defined. Set property");
+		}
+		
+		resp.sendRedirect(target);
+	}
+}
