78b3a41fde5923402eae9406c0ff0db1c5a3802e
Dario Susnisky <dario15th@gmail.com>
2012-07-12 16:16:13 -0300

    Forgot to push the exception :D

---
+++ src/main/java/ar/edu/itba/paw/grupo1/model/ModelNotValidException.java
@@ -0,0 +1,14 @@
+package ar.edu.itba.paw.grupo1.model;
+
+public class ModelNotValidException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7658652422356547119L;
+
+	
+	public ModelNotValidException() {
+		// TODO Auto-generated constructor stub
+	}
+}
